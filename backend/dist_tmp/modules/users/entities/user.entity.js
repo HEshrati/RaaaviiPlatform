576e855f124dc3e015b294729daf61c2250f302d
HEshrati <heshrti200416@gmail.com>
2026-08-05 20:03:32 +0330

    ssss

--- backend/dist_tmp/modules/users/entities/user.entity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-// این فایل از entity اصلی re-export می‌کند تا تضاد نباشه
-var user_entity_1 = require("../.././../database/entities/user.entity");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_entity_1.User; } });
